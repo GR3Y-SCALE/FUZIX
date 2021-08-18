56a18d2bee2003d259ef60dcaf0760f52768f4ef
GR3Y-SCALE <evanfoxx18@gmail.com>
2021-08-19 00:45:52 +0930

    added function array

---
+++ Source/Kernel/platform-rpipico/keyboard.c
@@ -0,0 +1,7 @@
+char keyboard[2][9] = {
+  {"Qq1","Ww2","Ee3","Rr4","Tt5","Yy6","Uu7","Ii8","Oo9","Pp0",null},
+  {null,"Aa!","Ss@","Dd#","Ff$","Gg%","Hh+","Jj&","Kk*","Ll-",null},
+  {null,"Zz(","Xx)","Cc?","Vv/","Bb'","Nn;","Mm:",".,,",null,null}
+}
+
+char functionKeys[4] = {"SPC","FN","ESC","DEL","ENT"}
